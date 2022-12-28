cf61e6529317f7d0cbfc70e7e7ffedaef0e79d92
We2Am-BaSsem <weaam.wewe91@gmail.com>
2022-12-28 15:19:26 -0800

    make robot move - works wrongly but work

--- run_rVis.sh
+++ run_rVis.sh
@@ -1,3 +1,4 @@
 #! /bin/bash
 
+catkin_make
 roslaunch summit_xl_sim_bringup summit_xls_complete.launch
--- src/our_package/scripts/my_first_node.py
+++ src/our_package/scripts/my_first_node.py
@@ -8,11 +8,11 @@ from turtlesim.msg import Pose
 
 def main():
     node_name = "input_listener"
-    topic = "/robot/input"
+    topic = "/robot/cmd_vel"
         
     # initiate the node and create a publisher
     rospy.init_node(node_name, anonymous=True)
-    pub = rospy.Publisher("/turtle1/cmd_vel", Twist, queue_size=10)
+    pub = rospy.Publisher(topic, Twist, queue_size=10)
     rospy.loginfo("%s is now running", node_name)
     
     rate = rospy.Rate(10) # 10hz
@@ -35,12 +35,6 @@ def main():
         except:
             user_input = key.name  # other keys
         if user_input not in ['a', 'd', 'w', 's']:
-            msg.linear.x = 0.0
-            msg.linear.y = 0.0
-            msg.linear.z = 0.0
-            msg.angular.x = 0.0
-            msg.angular.y = 0.0
-            msg.angular.z = 0.0
             return False
         if user_input == 'a':
             # left
@@ -68,16 +62,22 @@ def main():
         return False  # stop listener
         
     def on_release(key):
-        # msg.linear.x = 0.0
-        # msg.linear.y = 0.0
-        # msg.linear.z = 0.0
-        # msg.angular.x = 0.0
-        # msg.angular.y = 0.0
-        # msg.angular.z = 0.0
+        msg.linear.x = 0.0
+        msg.linear.y = 0.0
+        msg.linear.z = 0.0
+        msg.angular.x = 0.0
+        msg.angular.y = 0.0
+        msg.angular.z = 0.0
         return False  # stop listener
         pass
     
     while not rospy.is_shutdown():
+        msg.linear.x = 0.0
+        msg.linear.y = 0.0
+        msg.linear.z = 0.0
+        msg.angular.x = 0.0
+        msg.angular.y = 0.0
+        msg.angular.z = 0.0
         listener = keyboard.Listener(on_press=on_press, on_release=on_release)
         listener.start()  # start to listen on a separate thread
         listener.join()  # remove if main thread is polling self.keys
